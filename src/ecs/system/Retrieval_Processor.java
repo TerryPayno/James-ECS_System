365aa1783c60c0fdc963861cfb08900470fc4169
jwepa <jwepa@10.1.196.41>
2016-11-07 20:30:18 +0000

    Version 0.2

---
+++ src/ecs/system/Retrieval_Processor.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ecs.system;
+//@@@@@@@@@@ Processor that will retrive information from the database @@@@@@@@@@@//
+/**
+ *
+ * @author jwepa
+ */
+public class Retrieval_Processor {
+    
+public void GetData(){
+    
+}
+}
---
+++ src/ecs/system/Student_Processor.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+//@@@@@@@ This is where any info about the student will be added to  the coversheet.@@@@@@@//
+package ecs.system;
+
+/**
+ *
+ * @author jwepa
+ */
+public class Student_Processor {
+    int studentID;
+    String studentFName;
+    String studentLName;
+/*public Student_Processor(int ID, String fName,String lName){
+    studentID = ID;
+    studentFName = fName;
+    studentLName = lName;
+}*/
+public void editInfo(int ID, String fName, String lName){
+    studentID = ID;
+    studentFName = fName;
+    studentLName = lName;
+}
+public int getID(){
+    return studentID;
+}
+public String getFName(){
+    return studentFName;
+ }
+public String getLName(){
+    return studentLName;
+}
+
+
+}
+
